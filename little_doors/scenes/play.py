094e9cc2df9c8db6e2f0cc8a1c9ac87dffcbdde3
Willem Victor <wimpievictor@gmail.com>
2020-01-04 19:07:37 +0200

    Added bounding box grid index to play scene

--- little_doors/scenes/play.py
+++ little_doors/scenes/play.py
@@ -2,6 +2,7 @@ from pyglet import gl
 
 from little_doors import data
 from little_doors.camera import PixelCamera, pyglet
+from little_doors.grid import GridIndex2D
 from little_doors.iso import cart_to_iso
 from little_doors.player import Player
 from little_doors.scene import Scene
@@ -13,6 +14,9 @@ class PlayScene(Scene):
         self.terrain = Terrain((8, 8))
         self.camera = PixelCamera()
         self.player = Player(pos3d=(0.0, 0.0, 1.0))
+
+        # Bounding box indexes
+        self.static_grid = GridIndex2D(position=(-1024, -1024), dimensions=(64, 64), cell_size=(32.0, 32.0))
 
         # Text labels for cartesian coordinates
         (tile_width, tile_height) = self.terrain.tile_size_2d
@@ -41,6 +45,12 @@ class PlayScene(Scene):
             1, 0, 0, 1, 1, 0, 0, 1,
             1, 1, 1, 1, 1, 1, 1, 1,
         ])
+
+        # Index bounding boxes that are not expected to change.
+        for x, y in self.terrain:
+            aabb2d = self.terrain.get_cell_aabb2d(x, y)
+            if aabb2d is not None:
+                self.static_grid.insert(aabb2d)
 
     def on_update(self, dt):
         self.player.update(dt)
